6c6cc0ec2a48877909d1239864064bc2a1bc1877
Venkat Gorla <venkatagorla@gmail.com>
2026-01-22 15:12:58 +0530

    [in progress] silver layer spark changes to create partitioned Parquet - pending testing

--- glue/scripts/raw_to_curated_orders.py
+++ glue/scripts/raw_to_curated_orders.py
@@ -1,6 +1,6 @@
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, to_date, year, month, dayofmonth
 
 sc = SparkContext()
 glue_context = GlueContext(sc)
@@ -13,6 +13,18 @@ dyf = glue_context.create_dynamic_frame.from_catalog(
 
 df = dyf.toDF()
 
+# Parse order_date for partitioning
+df = df.withColumn(
+    "order_date",
+    to_date(col("order_date"))
+)
+
+# Derive partition columns
+df = df \
+    .withColumn("year", year(col("order_date"))) \
+    .withColumn("month", month(col("order_date"))) \
+    .withColumn("day", dayofmonth(col("order_date")))
+
 # Silver schema enforcement:
 # - amount is cast to decimal for correct analytics semantics
 df_silver = df.withColumn(
@@ -20,7 +32,8 @@ df_silver = df.withColumn(
     col("amount").cast("decimal(10,2)")
 )
 
-# Write to S3 as Parquet (Silver)
+# Write to S3 as partitioned Parquet (Silver)
 df_silver.write \
     .mode("overwrite") \
+    .partitionBy("year", "month", "day") \
     .parquet("s3://aws-glue-lakehouse-vegorla/curated/orders/")
